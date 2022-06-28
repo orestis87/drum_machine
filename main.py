b22ae80e1bc29108d62986c1bc1ffc4a6f8987a5
Orestis Nalmpantis <onalmpantis@suse.com>
2022-06-28 12:52:20 +0200

    set up the main screen

--- main.py
+++ main.py
@@ -15,3 +15,13 @@ gray = (128, 128, 128)
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption('SUSE Drum Machine')
 
+# Adding custom font
+label_font = pygame.font.Font('Snack bowl.otf', 32)
+
+fps = 60
+timer = pygame.time.Clock()
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill(black)
