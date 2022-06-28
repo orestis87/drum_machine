a0a9c757888cdf4f7c0b7aa0a224218a27915c33
Orestis Nalmpantis <onalmpantis@suse.com>
2022-06-28 12:42:36 +0200

    initial setup

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pygame
-print ("orestis")
+from pygame import mixer
+pygame.init()
+
+# Setting up the screen
+WIDTH = 1400
+HEIGHT = 800
+
+# Defining RGB colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+gray = (128, 128, 128)
+
+# Creating the screen
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('SUSE Drum Machine')
+
